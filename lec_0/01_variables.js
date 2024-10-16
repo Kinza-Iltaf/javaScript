b5855c3c582140a936d44f3c634fd022ebcc2e7b
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-10-16 09:00:32 +0000

    updation

---
+++ lec_0/01_variables.js
@@ -0,0 +1,24 @@
+// declarion of variable and constants
+const accountId = 1234;
+var balance = 2352525;
+let accountTitle = "Adam";
+branch = "New York";
+
+// printing on console
+console.log(accountTitle);
+
+// Trying to change the constant type of variable
+
+// accountId = 23 not possible to change the constant type of variable
+
+console.log(accountId);
+
+//  var and let both are variables but the var does not have any block or functional scope so if we have name variable in one function and we declare another variable having name "name" so when we initialize that it will change the name variable value of the above variable 
+
+
+// printing variables in form of talbe
+
+
+console.table([accountId,balance, accountTitle, branch])
+
+
