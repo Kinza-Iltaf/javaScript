63a08a4835abebcb007d613b5a858932793211b8
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-10-19 20:15:18 +0000

    dataTypes

---
+++ lec_0/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" // now the js code will be treat as the newer js code like latest js version code.
+// varialble declaration
+let name = " Adam "
+let age = 23
+let isLoggedIn = true
+
+console.table([name,age, isLoggedIn])
+
+// some data types
+// number
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined => not define a value means variable is declared but does not assigned any value to it.
+// symbol => when we are talking about unique value
+// object
+console.log(age, typeof(age))
