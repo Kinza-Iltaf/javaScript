ae155d6a2343adb84e2f4a5079d1e6dc798ee02b
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-10-19 20:42:04 +0000

    Typecasting/coverstion of variable

---
+++ lec_0/03_conversionOperations.js
@@ -0,0 +1,10 @@
+let score = 34
+console.log(typeof score)
+console.log(typeof(score))
+
+// 33 => numbers
+// 33abc => numbers (confussing but its type is number)
+// true =>1
+// false => 0
+// typeconversion
+// numbers conversion, string conversion, boolean converstion
