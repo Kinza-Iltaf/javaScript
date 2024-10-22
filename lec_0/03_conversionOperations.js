d8846b8596841f601ba5ecbc935156548ccd90ea
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-10-22 19:54:28 +0000

    operations

--- lec_0/03_conversionOperations.js
+++ lec_0/03_conversionOperations.js
@@ -8,3 +8,13 @@ console.log(typeof(score))
 // false => 0
 // typeconversion
 // numbers conversion, string conversion, boolean converstion
+// ######## Operations ########
+// append
+s1 = "Hello"
+s2 = " John"
+s3 = s1 + s2
+console.log(s3)
+
+// if we are performing different operatioin in same paranthesis then the the rest numbers will be considered according to first number.
+console.log(2+3+"3")   //treat as numbers
+console.log("2"+3+3)  // treat as string
