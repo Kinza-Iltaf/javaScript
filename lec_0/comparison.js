2b9f20853bbd04f161eb06991132223415d23e0f
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-10-24 19:32:52 +0000

    updation

--- lec_0/comparison.js
+++ lec_0/comparison.js
@@ -14,4 +14,8 @@ console.log("2">1)
 console.log(null == 0)
 // in equality it treat the null as number
 console.log(null>=7)
-// and in comparison it is converted to 0 first then its checked
+// and in comparison it is converted to 0 first then its checked 
+
+// strict conversion
+// now at time of converstion the datatype would also be checked if not same error would occure.
+console.log("2"===2)
