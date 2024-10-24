c86ad1044004ad6ee1f7ea72c006c1ca28317a8c
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-10-24 18:57:05 +0000

    comparison of same and different datatypes

---
+++ lec_0/comparison.js
@@ -0,0 +1,17 @@
+//simple comparison
+console.log(2>1)
+console.log(1<2)
+console.log(2==4)
+console.log(2==4)
+console.log(2<=4)
+console.log(2>=4)
+console.log(2!=4)
+
+// it become difficult when we are comparing two different type of variable like string and integer.
+
+console.log("2">1)
+
+console.log(null == 0)
+// in equality it treat the null as number
+console.log(null>=7)
+// and in comparison it is converted to 0 first then its checked
