d6f4ba4a7937ebc6e6f126ca87fe37daef7a916f
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-10-27 19:27:03 +0000

    string concepts

---
+++ lec_0/05_String.js
@@ -0,0 +1,16 @@
+// string declaration and cancatenation
+const f_name = "John"
+const s_name = "Ibrahim"
+
+console.log(f_name + s_name + "Vemo")
+
+// But it is outdated method
+
+// for modern way we use string interpolation in which we use the backticks (``)
+
+console.log(`Hello ${f_name.toUpperCase()} ${s_name} to our program`)
+
+// string declaration in new way
+
+const myName = new String("Mike")
+console.log(myName)
