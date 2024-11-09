3f70a4a63f47e01521006cf0c96613de86a1b54c
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-11-09 18:56:29 +0000

    update

--- lec_0/06_num_and_maths.js
+++ lec_0/06_num_and_maths.js
@@ -12,4 +12,27 @@ console.log(price.toPrecision(4)) //focus on digit before the point
 const population = 1209080000;
 console.log(population.toLocaleString("en-PK"));  //to easily read a huge number by putting commas between them
 
+// ####### Math  ###########
+console.log(Math);
+const img_num = -23
+console.log(Math.abs(img_num)) //change from negative to positive
+
+console.log(Math.round(4.7))
+console.log(Math.ceil(4.7))
+console.log(Math.floor(4.7))
+
+console.log(Math.min(0,5,2,6,2))
+console.log(Math.max(0,5,2,6,2))
+console.log(Math.pow(2,3))
+
+console.log(Math.random()) //it value will always be a random value from 1 to 0.
+
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1) + min))
+
+
 
