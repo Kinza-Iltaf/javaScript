1dd952b6af2cf4cfc726c20afe9b0777b4f41557
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-11-09 12:14:33 +0000

    addition of new concepts

--- lec_0/06_num_and_maths.js
+++ lec_0/06_num_and_maths.js
@@ -4,3 +4,8 @@ console.log(balance)
 //simple number type of data
 const score = 342
 console.log(score)
+console.log(score.toString().length)
+const price = 234.6342
+console.log(price.toFixed(2)) // round off the values after point
+console.log(price.toPrecision(4)) //focus on digit before the point
+
