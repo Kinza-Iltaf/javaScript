ce7f04460dce133594281388c147dda025295b9f
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-11-11 19:34:33 +0000

    update

--- lec_0/07_Date_in_js.js
+++ lec_0/07_Date_in_js.js
@@ -1,2 +1,7 @@
-const myDate = new Date();
-console.log(myDate);
+const myDate = new Date(2004, 2, 1 , 7 , 6);
+// console.log(myDate);
+
+//conversion into string
+console.log(myDate.toDateString());
+
+
