679b9f8e66d2a751a48f4404fa45bd74f76bc6bb
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-11-30 20:47:27 +0000

    Addition of array

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,14 @@
+const myArr = [0,2,6,2,61,5];
+//accessing Array all element at once
+console.log(myArr);
+
+//accessing specific element of array at specific index
+
+// console.log(myArr[3]);
+
+//array methods
+myArr.push(23); //add Element to array at the end
+myArr.push(124)
+console.log(myArr);
+myArr.pop();//remove last value
+console.log(myArr);
