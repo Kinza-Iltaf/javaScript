1d46590d890c9c7713223457cf7421de477ebd10
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-11-30 21:06:03 +0000

    Array methods

--- 02_basics/01_Array.js
+++ 02_basics/01_Array.js
@@ -9,6 +9,22 @@ console.log(myArr);
 //array methods
 myArr.push(23); //add Element to array at the end
 myArr.push(124)
-console.log(myArr);
+// console.log(myArr);
 myArr.pop();//remove last value
+// console.log(myArr);
+myArr.unshift(12342); //shift all the array element and add the new element to the start but if arrray has thousand values so it will become a time consuming process which will load our system so it is not prefarable to use.
+
+// console.log(myArr);
+myArr.shift(); //remove first element of array
+// console.log(myArr);
+
+console.log(myArr.includes(0)); //check for the mentioned element that it exist or not in array
+
+// console.log(myArr.indexOf(61));
+
+const newArr = myArr.join(); //convert the array into string
+// console.log(newArr);
+console.log(myArr);
+console.log(myArr.slice(1,4));
+console.log(myArr.splice(1,4)); //it also include the last element but it manipulate the original array too.
 console.log(myArr);
