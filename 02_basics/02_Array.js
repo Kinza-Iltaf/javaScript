f20032b5e46a9099ec1e25b54651ea22c58dc504
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-12-01 20:44:39 +0000

    new content related to array

---
+++ 02_basics/02_Array.js
@@ -0,0 +1,13 @@
+//merging two arrays
+const evenNum = [0,2,4,6];
+const oddNum = [ 1,3,5,7];
+
+//merging: the next array will be add as single element of an array
+// evenNum.push(oddNum);
+
+// console.log(evenNum.length);
+
+//contactination
+
+const newArr = evenNum.concat(oddNum);
+console.log(newArr);
