b779f2084e57ad6db2cf77764be4387796ef688c
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-12-07 17:56:01 +0000

    new_concept

--- 02_basics/02_Array.js
+++ 02_basics/02_Array.js
@@ -10,5 +10,14 @@ const oddNum = [ 1,3,5,7];
 //contactination
 
 const newArr = evenNum.concat(oddNum);
-console.log(newArr);.
+// console.log(newArr);
+
+//spread method... used to cantact more then two arrays. element the limitation of cantact method.
+const mix_num = [23,52,51];
+const new_num = [...evenNum,...oddNum,...mix_num];
+// console.log(new_num);
+
+const new_method = [2, [4, 5 , 34,6,2], 3 , 5, [45, 34, [35, 25,6]]];
+const flat_arr = new_method.flat(1);
+console.log(flat_arr);
 
