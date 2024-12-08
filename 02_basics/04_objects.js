a712d97f48b74fa2b8ac541340555964b3f27b6f
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-12-08 21:52:18 +0000

    singleton_object concepts

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,8 @@
+//objects singleton (how to declare objects through constructor)
+
+//literal object
+const user_info = {};
+//singleton object
+const use_info = new Object();
+console.log(user_info)
+console.log(use_info)
