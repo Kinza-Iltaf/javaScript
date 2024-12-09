dd66f2469021c8db7e3ff0aa37b8ec0925d34014
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-12-09 18:30:50 +0000

    nesting of objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,8 +1,31 @@
 //objects singleton (how to declare objects through constructor)
 
 //literal object
-const user_info = {};
+const use_info = {};
 //singleton object
-const use_info = new Object();
-console.log(user_info)
-console.log(use_info)
+const user_info = new Object();
+// console.log(user_info)
+// console.log(use_info)
+
+//simple way
+user_info.id = "123abc";
+user_info.gmail = "someone@gmail.com";
+user_info.isAvailable = false;
+
+console.log(user_info);
+
+//objects inside another object
+
+const user = {
+    age : 23,
+    gmail :"someone@gmail.com",
+    user_name : {
+        full_name: {
+            first_name : "Adam",
+            last_name : "smith"
+
+        }
+    }
+}
+
+console.log(user.user_name.full_name.last_name)
