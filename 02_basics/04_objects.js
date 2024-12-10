6b4f27311489c360200463ae7e0d436196ecc10c
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-12-10 03:50:15 +0000

    objects methods

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -12,7 +12,7 @@ user_info.id = "123abc";
 user_info.gmail = "someone@gmail.com";
 user_info.isAvailable = false;
 
-console.log(user_info);
+// console.log(user_info);
 
 //objects inside another object
 
@@ -28,4 +28,26 @@ const user = {
     }
 }
 
-console.log(user.user_name.full_name.last_name)
+// console.log(user.user_name.full_name.last_name)
+
+// const full_details = {user_info,user};
+//combining two objects
+
+// const obj1 ={
+//     1: "a",
+//     2 :"b"
+// }
+
+// const obj2 ={
+//     3: "a",
+//     4 :"b"
+// }
+// const full_details = Object.assign ({},user_info,user);
+
+const full_details = {...user,...user_info};
+
+
+// first object is target where all the rest of the objects will be stored.
+console.log(full_details);
+
+
