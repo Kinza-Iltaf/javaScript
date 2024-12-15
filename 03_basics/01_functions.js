a1172cc904e3a6b0d0059932283ff0b35fcfb36e
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2024-12-15 20:03:58 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+function addition(num1 ,num2) // num1 and num2 are parameters
+{
+    // console.log(num1+num2)
+}
+
+// addition(3, 6) 
+//3 and 6 are here arguments
+
+//storing function value in variable
+
+var addition =  addition(2,5);
+console.log(addition) //give undefined because function does not return any value
+
+function addition2(num1 ,num2) // num1 and num2 are parameters
+{
+    var result = num1+num2;
+    return result;
+}
+
+var add = addition2(1,5);
+// console.log (add);
+
+
