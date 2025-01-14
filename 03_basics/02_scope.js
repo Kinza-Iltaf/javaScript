2a89443f11c7c265b59cef05015bbc35bf062211
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-01-14 20:10:00 +0000

    scope in nested functions and loops

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -16,3 +16,18 @@ const password = 234
 //outside of these paranthesis is global scope and it it available inside the block scope and the block scope is not available in global scope
 
 //now all variable has scope and can not be access outside the paranthesis except the var we can access it from anywhere in our program
+
+//if we have nested functions or loops or conditional statements then the outer variable can be accessed by the inner loop or functions but the inner variable can not be used outside of paranthesis
+
+function add(){
+    let num1 = 2
+    function display(){
+        let num2 = 3
+        console.log(num1)
+    }
+
+    // console.log(num2)
+    display()
+}
+
+add()
