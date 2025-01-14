f4e98c730f1b70c1f8860b3dad9ea2ede9c5d753
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-01-14 19:40:09 +0000

    scope of the variables

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,18 @@
+//variables
+let number = 10
+var age = 23
+const password = 234
+
+// {} this is scope
+
+if(true){
+
+    let number = 10
+var age = 23
+const password = 234
+
+} //inside of this is block scope 
+
+//outside of these paranthesis is global scope and it it available inside the block scope and the block scope is not available in global scope
+
+//now all variable has scope and can not be access outside the paranthesis except the var we can access it from anywhere in our program
