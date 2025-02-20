222be3743b9914714d6e2c800309a166d59b1557
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-02-20 13:22:08 +0000

    random function summary and how its work

--- lec_0/06_num_and_maths.js
+++ lec_0/06_num_and_maths.js
@@ -1,12 +1,20 @@
 //defining new object of number type specifically....
+
+
 const balance = new Number(2000871)
 console.log(balance)
+
 //simple number type of data
 const score = 342
 console.log(score)
+
+//conversion to string to apply method on it
 console.log(score.toString().length)
 const price = 234.6342
+
+
 console.log(price.toFixed(2)) // round off the values after point
+
 console.log(price.toPrecision(4)) //focus on digit before the point
 
 const population = 1209080000;
@@ -27,8 +35,13 @@ console.log(Math.pow(2,3))
 
 console.log(Math.random()) //it value will always be a random value from 1 to 0.
 
+//we the values we will get will be 0.1, 0.2 and so on so for this we multiply 10
+//now the range is 0 to 9
+// so we can get 0 as well, to avoid this we add 1 and 
+
 console.log((Math.random()*10)+1)
 
+//for when range is given
 const min = 10
 const max = 20
 
