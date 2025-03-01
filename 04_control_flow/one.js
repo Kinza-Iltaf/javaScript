302cdaa71ea0d0729041cf1851369db7909263f6
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-03-01 13:31:02 +0000

    control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+//conditional statement
+
+//the inside code will execute when the specific condition meet
+
+// use comparison operator 
+
+// = (assign), == (equal) , === (value and type checking) , > , < , >= , <= , != , !==(check the value and type that is not equal)etc
+
+if(2 !== "2"){
+    //console.log(`Hi m here! condition is true`);
+    
+}
+else{
+    //console.log(`condition is false`);
+    
+}
+
+//scopes 1. Global scope  2.block scope 3.implecit scope (one line scope)
+
+let score = 10;
+if(score >5) console.log(`Pass. Your score is  ${score}`);
+
+//here we can write multiple lines as well but we have to use the comman between lines and at the end semicolon but its not a good practice
+
+//multiple conditions if_else 
+
+//&& (and)  || (or) pipe sign
+
+
