ccf7a6f68de517e3b1e3baa012d79fce306f29cf
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-03-01 18:32:49 +0000

    all about control flow

---
+++ 04_control_flow/Truthy_Falsy_values.js
@@ -0,0 +1,26 @@
+//Truthy Values => which will always gives us true value
+
+//non_empty string, non_zero number (it can be negative values as well), empty object and array , function, date_object  , "false" its also now string , " " there is space so its also truthy value , "0" again string
+
+if(-2){
+    //console.log("not falsy value");
+    
+}
+else{
+    //console.log("falsy value");
+    
+}
+
+//falsy value 
+
+//false , 0 , -0(interview) , empty string , null , undefined , BigInt 0n etc
+
+let name; //practical for undefined value
+if(name){
+    //console.log("Not falsy value");
+    
+}
+else{
+    //console.log("falsy value");
+    
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,18 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//important Element of the switch statement
+//key => variable which want to match everytime
+
+//case => different possibilities
+
+//break => condition meet then go outside from the switch statment
+
+// not use the break => all lines will execute except default statement
+
