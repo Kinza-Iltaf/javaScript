b31c1abacf895ea2fae04fc8d5a38405ca00b626
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-03-03 02:04:39 +0000

    practice of for loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,3 +1,24 @@
 //for loop
 
 //steps : 1.initialization 2.condition check 3.body execution 4.increment (depends (if prefix ++i (then increment will take place before execution)))
+
+for (let index = 0; index < 11; index++) {
+   // console.log(index); // 0 to 10
+       
+}
+
+//for duplication : ctrl+d
+
+//lets try nested loop
+
+for (let i = 0; i <=10; i++) {
+  // console.log(`Table of ${i}`);
+   for (let j = 0; j <=10; j++) {
+  // console.log(`${i}` + `*` + `${j}` + `=` + `${i*j}` )
+   
+   }
+      
+}
+
+
+
