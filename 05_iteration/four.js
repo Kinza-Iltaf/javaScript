c837a5d7fc3a989f40924394b6ef4d6ca00719fe
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-03-05 02:34:20 +0000

    for-in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,22 @@
+//loops on object
+
+const PakistanProvinces = {
+    province1 : "KPK",
+    province2 : "Punjab",
+    province3 :  "sindh",
+    province4 : "Balochistan",
+}
+
+for (const key in PakistanProvinces) {
+   //console.log(key);//will return only keys
+   //console.log(PakistanProvinces[key])
+  // console.log(`${key} ${PakistanProvinces[key]}`);  //key value pair
+    }
+
+    //forof loop => where we are interested in values not keys like array, string
+
+    //forin => where we want keys and values as well like objects not for map
+
+    
+
+    
