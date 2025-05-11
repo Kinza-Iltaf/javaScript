42c25df83a76941f4227e3e62207e9be6994337b
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-05-11 19:48:15 +0000

    then, catch and finally in promises

--- 09_advance1/promises.js
+++ 09_advance1/promises.js
@@ -27,3 +27,40 @@ new Promise(function(resolve,reject){
 }).then(function(){
     console.log("promise consumed:");
 })
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({userName: "Adam", email : "google@gmail.com"})
+    },2000)
+})
+
+promiseThree.then(function(user){
+    console.log("User Data is:", user)
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        let error = false;
+        if(!error){
+            resolve({userName: "James", Password: "1234"})
+        }
+        else{
+            reject("Error:Something went Wrong:")
+        }
+    },1000)
+})
+
+promiseFour
+.then((user)=>{
+    console.log(user);
+    return user.Password
+})
+.then((Password) => {
+    console.log(Password);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(()=>{
+    console.log("The promise is either resolve or rejected:")
+})
