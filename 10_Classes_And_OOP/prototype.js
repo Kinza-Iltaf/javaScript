14a21a5fa2ac45859a410beb787d48c94593b560
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-05-28 19:11:28 +0000

    prototype in js

---
+++ 10_Classes_And_OOP/prototype.js
@@ -0,0 +1,4 @@
+//prototype : JS has prototypal behavior 
+//matlab har nahy manty koi value dondny main upper jaty hain parent main pr grandparent main jab tak k osy null na mil jay
+
+//this key word b esee se link ho kar kam karta hain or arrow function main jo this nahy hota wo b prototype ke waja se nahy hota
